863648b8b0949ddf4cd9ce7f3adcb33db58a5e25
Matthew Zenn <spyguy57@gmail.com>
2024-03-03 15:37:20 -0500

    initialized script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KryptosJS
-A javascript library that gereate random, but syntaxically correct javascript code
+A javascript library that generates random, but syntaxically correct javascript code.
+![screen](screenshot.png)
+<br>
+I plan to add more variety to the code and figure out a way to format it. A possible implementation for this may be in a text art generator. 
---
+++ kryptos.js
@@ -0,0 +1,48 @@
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var string_len = 0;
+var string = '';
+var string2 = '';
+var num = 0;
+var output = '';
+
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+
+export function gen(length) {
+  output = '';
+  for (i=0; i<length; i++) {
+    string = '';
+    string2 = ''; 
+    num = Math.floor(Math.random() * (50 - 0 + 1) + 0);
+    string_len = Math.floor(Math.random() * (12 - 5 + 1) + 5);
+    for (k=0; k<string_len; k++) {
+      string += letters.random();
+    }
+    string_len = Math.floor(Math.random() * (12 - 5 + 1) + 5);
+    for (k=0; k<string_len; k++) {
+      string2 += letters.random();
+    }
+    let number = Math.floor(Math.random() * 5) + 1;
+    switch(number) {
+      case 1:
+        output += "var "+string+" = "+num+";";
+        break;
+      case 2:
+        output += "const "+string+" = "+num+";";
+        break;
+      case 3:
+        output += "document.querySelector("+string+").selectionStyle("+string2+");";
+        break;
+      case 4:
+        output += "document.getElementById("+string+").innerHTML == "+string2+";";
+        break;
+      case 5:
+        output += "var "+string+" = Math.random();";
+        break;
+    }
+  }
+}
+
+gen(50);
+console.log(output);
